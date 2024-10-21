c36846497b1793800ab47a18d0130a8c76f25d96
Lexicon <iyiolad21@gmail.com>
2024-10-21 23:15:31 +0100

    refactor: Update Admin.jsx and add AdminHeader component

--- client/src/pages/admin-view/Admin.jsx
+++ client/src/pages/admin-view/Admin.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import AdminHome from './AdminHome'
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import AdminLayout from "./Layout";
+import AdminDashboard from "./Dashboard";
+import AdminProducts from "./Products";
+import AdminOrder from "./Order";
+import AdminFeatures from "./Features";
 
 const Admin = () => {
   return (
     <div>
-        <h1>Admin Dashboard</h1>
-        <Routes>
-            <Route path="/" element={<Navigate to="/admin/adminHome"/>}/>
-            <Route path='/adminHome' element={<AdminHome/>}/>
-        </Routes>
+      <h1>Admin Dashboard</h1>
+      <Routes>
+        <Route path="/" element={<Navigate to="/admin/AdminLayout" />} />
+        <Route path="/AdminLayout" element={<AdminLayout />} />
+        <Route path="/dashboard" element={<AdminDashboard/>}/>
+        <Route path="/product" element={<AdminProducts/>}/>
+        <Route path="/orders" element={<AdminOrder/>}/>
+        <Route path="/features" element={<AdminFeatures/>}/>
+        <Route/>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
