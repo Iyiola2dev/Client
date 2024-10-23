e57a607f4e17e132757277da8b63e2d04c6ac641
Lexicon <iyiolad21@gmail.com>
2024-10-23 11:25:24 +0100

    Add register link to LandingPage  just for testing the routes

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div>LandingPage
+      <Link to="/auth/register">register</Link>
+    </div>
   )
 }
 
