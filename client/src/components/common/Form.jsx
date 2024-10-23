0ded56f7120a08be73ab989aa90ce8c1de9c6e88
Lexicon <iyiolad21@gmail.com>
2024-10-23 11:28:14 +0100

    Refactor Form component to include input, select, and textarea elements

--- client/src/components/common/Form.jsx
+++ client/src/components/common/Form.jsx
@@ -1,9 +1,128 @@
-import React from 'react'
+import React from "react";
+import { Input } from "../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { Textarea } from "../ui/textarea";
+import { Button } from "../ui/button";
 
-const CommonForm = () => {
+const CommonForm = ({
+  formControls,
+  formData,
+  setFormData,
+  onSubmit,
+  buttonText,
+}) => {
+  function renderInputsByComponentType(getControlItem) {
+    let element = null;
+    //This is the value of the form data which will consist of all the key values that this form control will have
+    const value = formData[getControlItem.name || ""];
+
+    switch (getControlItem.component) {
+      case "input":
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+            value={value}
+           
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: e.target.value,
+              })
+            }
+          />
+        );
+        break;
+      case "select":
+        element = (
+          //The select component is from shacdn-ui the selectTrigger, SelectValue, SelectContent, SelectItem are all components from shacdn-ui which have their own functionality...
+          <Select
+          //This is to handle the change of the select value
+            onValueChange={(value) =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: value,
+              })
+            }
+            value={value}
+          >
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder={getControlItem.placeholder} />
+            </SelectTrigger>
+            <SelectContent>
+              {getControlItem.options && getControlItem.options.length > 0
+                ? getControlItem.options.map((option) => (
+                    <SelectItem key={optionItem.id} value={optionItem.id}>
+                      {option.label}
+                    </SelectItem>
+                  ))
+                : null}
+            </SelectContent>
+          </Select>
+        );
+        break;
+      case "textarea":
+        element = (
+          //The input component is from shacdn-ui
+          <Textarea
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.id}
+            value={value}
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: e.target.value,
+              })
+            }
+          />
+        );
+        break;
+      default:
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+            value={value}
+            //if you ever wonder where the setFormData is coming from, it is from the parent component and I'm using redux toolkit to manage the state
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: e.target.value,
+              })
+            }
+          />
+        );
+    }
+    return element;
+  }
   return (
-   <form action=""></form>
-  )
-}
+    <form onSubmit={onSubmit}>
+      <div className="flex flex-col gap-3">
+        {formControls.map((controlItem) => (
+          <div className="grid w-full gap-1.5" key={controlItem.name}>
+            <label className="mb-1" htmlFor={controlItem.name}>
+              {controlItem.label}
+            </label>
+            {renderInputsByComponentType(controlItem)}
+          </div>
+        ))}
+      </div>
+      <Button type="submit" className="mt-2 w-full">
+        {buttonText || "Submit"}
+      </Button>
+    </form>
+  );
+};
 
-export default CommonForm
+export default CommonForm;
---
+++ client/src/config/Index.jsx
@@ -0,0 +1,34 @@
+
+//this is our form control configuration file
+export const registerFormControls = [
+  {
+    name: "userName",
+  
+    placeholder: "Enter your username",
+    componentType: "input",
+    type: "text",
+    // options : []
+  },
+  {
+    name: "email",
+   
+    placeholder: "Enter your email",
+    componentType: "input",
+    type: "text",
+  },
+
+    {
+        name: "password",
+    
+        placeholder: "Enter your password",
+        componentType: "input",
+        type: "password",
+    },
+    // {
+    //     name: "confirmPassword",
+    //     label: "Confirm Password",
+    //     placeholder: "Confirm your password",
+    //     componentType: "input",
+    //     type: "password",
+    // },
+];
