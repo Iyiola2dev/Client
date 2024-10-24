3f293471784f02d28e96f89ca1540c16f62d8502
Lexicon <iyiolad21@gmail.com>
2024-10-24 05:53:19 +0100

    Refactor LandingPage to include images for visual appeal

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -3,8 +3,16 @@ import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage
-      <Link to="/auth/register">register</Link>
+    <div>
+      <main className='flex items-center min-h-full '>
+        <div className='flex-1 h-full'>
+          <img className='w-full max-w-sm object-cover object-center ' src="https://res.cloudinary.com/dtlejpoxq/image/upload/v1729744723/Mern-Ecommerce/side-view-smiley-doctor-work_1_lk12o1.png" alt="Smiley Doctor" />
+        </div>
+        <div className='flex-1'>
+        <img className='w-full max-w-sm object-cover' src="https://res.cloudinary.com/dtlejpoxq/image/upload/v1729744713/Mern-Ecommerce/giphy_1_shxv3d.png" alt="Animated Image" />
+        </div>
+      </main>
+     
     </div>
   )
 }
