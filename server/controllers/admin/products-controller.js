81d0589c7f8245ce038c8f0a7a15ab2206daf3b7
Lexicon <iyiolad21@gmail.com>
2024-10-28 15:57:01 +0100

    The product controller on what action it should perform

---
+++ server/controllers/admin/products-controller.js
@@ -0,0 +1,141 @@
+import { imageUploadUtil } from "../../helpers/cloudinary.js";
+
+export const handleImageUpload = async (req, res) => {
+  try {
+    //This is to check if there is no file uploaded
+    if (!req.file) {
+      return res.status(400).json({
+        success: false,
+        message: "No file uploaded",
+      });
+    }
+    //This is to convert to a based64
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    // const url = "data:" + req.file.mimetype + ";base64," + b64;
+    const url = `data:${req.file.mimetype};base64,${b64}`;
+    const result = await imageUploadUtil(url);
+
+    res.status(200).json({
+      success: true,
+      message: "Image uploaded successfully",
+      result,
+    });
+  } catch (err) {
+    console.error("Upload Error:", err);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+      error: err.message,
+    });
+  }
+};
+
+//add a new product
+export const addProduct = async (req, res) => {
+  try {
+    const { image, name, description, category, price, stock, sales } =
+      req.body;
+    const newProduct = new Product({
+      image,
+      name,
+      description,
+      category,
+      price,
+      stock,
+      sales,
+    });
+
+    await newProduct.save(); //This to save the new product to the database
+    res.status(201).json({
+      success: true,
+      data: newProduct,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
+
+//fetch all products from the database
+export const fetchAllProduct = async (req, res) => {
+  try {
+    const listOfProducts = await Product.find({}); //This is to fetch all products from the database
+    res.status.json({
+      success: true,
+      data: listOfProducts,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
+
+//edit a product
+export const editProduct = async (req, res) => {
+  try {
+    //I will be editing a product by id
+    const { id } = req.params;
+    const { image, name, description, category, price, stock, sales } =
+      req.body;
+
+    const findProduct = await Product.findById(id); //This is to find the product by id
+    if (!findProduct) {
+      res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    //This is to update the product
+    findProduct.name = name || findProduct.name;
+    findProduct.description = description || findProduct.description;
+    findProduct.category = category || findProduct.category;
+    findProduct.price = price || findProduct.price;
+    findProduct.image = image || findProduct.image;
+    findProduct.stock = stock || findProduct.stock;
+    findProduct.sales = sales || findProduct.sales;
+
+    await findProduct.save(); //This is to save the updated product
+    res.status(200).json({
+      success: true,
+      data: findProduct,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
+
+//delete a product
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByAndDelete(id); //This is to find the product by id and delete it
+
+    if (!product) {
+      res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+    })
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
