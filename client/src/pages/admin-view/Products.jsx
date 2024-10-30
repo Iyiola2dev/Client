9828450e0696fd5d3c8647ea10f3e8d426ec9fff
Lexicon <iyiolad21@gmail.com>
2024-10-30 16:11:39 +0100

    Refactor styling in Products component and add functionality to create and edit products

--- client/src/pages/admin-view/Products.jsx
+++ client/src/pages/admin-view/Products.jsx
@@ -1,5 +1,6 @@
 import CommonForm from "@/components/common/Form";
 import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
 import {
   Sheet,
   SheetContent,
@@ -7,8 +8,15 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { addProductFormElements } from "@/config/Index";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProductImageUpload from "./image-upload/Image-upload";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addNewProduct,
+  editProduct,
+  fetchAllProducts,
+} from "@/store/admin/products-slice";
+import AdminProductTile from "./admin-view2/Product-tile";
 const initialFormData = {
   image: null,
   name: "",
@@ -21,17 +29,68 @@ const initialFormData = {
 };
 
 const AdminProducts = () => {
+  //this is the state to open the create product form
   const [openCreateProduct, setOpenCreateProduct] = useState(false);
+  //this is the state to store the form data
   const [formData, setFormData] = useState(initialFormData);
+  //this is the state to store the image file
   const [imageFile, setImageFile] = useState(null);
+  //this is the state to store the uploaded image url
   const [uploadedImageURL, setUploadedImageURL] = useState("");
+  const { toast } = useToast();
+  //This is for the edit of product this state is to store the current id before it is edited
+  const [currentEditedId, setCurrentEditedId] = useState(null);
+
+  //this is to get the product list and the adminProduct is the reducer name
+  const { productList } = useSelector((state) => state.adminProducts);
+  const dispatch = useDispatch();
 
   //this is a loading state
-const [imageLoadingState, setImageLoadingState] = useState(false);
-  
-  function onSumbit() {
-    console.log(formData);
+  const [imageLoadingState, setImageLoadingState] = useState(false);
+
+  //this is to submit the form
+  function onSumbit(e) {
+    e.preventDefault();
+    //this is to check if the currentEditedId is true and i pass in the edited product Api from my async thunk
+    currentEditedId !== null
+      ? dispatch(editProduct({ id: currentEditedId, formData })).then(
+          (data) => {
+            console.log(data, "edit");
+            if (data?.payload?.success) {
+              dispatch(fetchAllProducts());
+              setFormData(initialFormData);
+              setOpenCreateProduct(false);
+              setCurrentEditedId(null);
+            }
+          }
+        )
+      : dispatch(addNewProduct({ ...formData, image: uploadedImageURL })).then(
+          (data) => {
+            console.log(data);
+            if (data?.payload?.success) {
+              //this if the sucess is true, the image file is set to null, the form data is set to the initial form data and the open create product is set to false basically to reset the form
+              dispatch(fetchAllProducts());
+              setImageFile(null);
+              setFormData(initialFormData);
+              setOpenCreateProduct(false);
+              toast({
+                title: "Product added successfully",
+              });
+            }
+          }
+        );
   }
+  function isFormValid() {
+    return Object.keys(formData)
+      .map((key) => formData[key] !== "")
+      .every((item) => item);
+  }
+
+  useEffect(() => {
+    dispatch(fetchAllProducts());
+  }, [dispatch]);
+
+  console.log(productList.data, uploadedImageURL, "productList");
   return (
     <>
       <div className="mb-5 flex w-full justify-end">
@@ -39,14 +98,38 @@ const [imageLoadingState, setImageLoadingState] = useState(false);
           Add New Product
         </Button>
       </div>
-      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4 "></div>
+      {/* This is the product tile card */}
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4 ">
+        {productList?.data?.length > 0 ? (
+          productList.data.map((productItem, i) => (
+            <AdminProductTile
+              key={i}
+              product={productItem}
+              setCurrentEditedId={setCurrentEditedId}
+              setOpenCreateProduct={setOpenCreateProduct}
+              setFormData={setFormData}
+            />
+          ))
+        ) : (
+          <p>No product found</p>
+        )}
+      </div>
+
       <Sheet
         open={openCreateProduct}
-        onOpenChange={() => setOpenCreateProduct(false)}
+        onOpenChange={() => {
+          setOpenCreateProduct(false);
+          //This is to make the dialog box to close and reset the form
+          setCurrentEditedId(null);
+          setFormData(initialFormData);
+        }}
       >
         <SheetContent side="right" className="overflow-auto">
           <SheetHeader>
-            <SheetTitle>Add New Product</SheetTitle>
+            <SheetTitle>
+              {/* This is to check if the currentEditedId is true The title shows true and if it's not  */}
+              {currentEditedId !== null ? "Edit Product" : "Add New Product"}
+            </SheetTitle>
           </SheetHeader>
           {/* The image upLoad component */}
           {/* The props is passed to the productimageupload component */}
@@ -54,8 +137,10 @@ const [imageLoadingState, setImageLoadingState] = useState(false);
             imageFile={imageFile}
             setImageFile={setImageFile}
             uploadedImageURL={uploadedImageURL}
-            setuploadedImageURL={setUploadedImageURL}
+            setUploadedImageURL={setUploadedImageURL}
             setImageLoadingState={setImageLoadingState}
+            imageLoadingState={imageLoadingState}
+            isEditMode={currentEditedId !== null}
           />
 
           {/* This is the commonform component */}
@@ -64,9 +149,10 @@ const [imageLoadingState, setImageLoadingState] = useState(false);
               formData={formData}
               setFormData={setFormData}
               formControls={addProductFormElements}
-              buttonText="Add Product"
+              buttonText={currentEditedId !== null ? "Update" : "Add Product"}
               borderRadius="rounded-md"
               onSubmit={onSumbit}
+              isBtnDisable = {!isFormValid()}
             />
           </div>
         </SheetContent>
