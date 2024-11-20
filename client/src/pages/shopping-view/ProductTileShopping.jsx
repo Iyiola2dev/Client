4bd9f571938008291a2e616feef47d714e847a58
Iyiola afuye <166384569+Iyiola2dev@users.noreply.github.com>
2024-11-20 09:38:23 +0100

    Add product details retrieval to ShoppingProductTile: implement click handler for product detail retrieval

--- client/src/pages/shopping-view/ProductTileShopping.jsx
+++ client/src/pages/shopping-view/ProductTileShopping.jsx
@@ -3,11 +3,14 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import React from "react";
 
-const ShoppingProductTile = ({ product }) => {
+const ShoppingProductTile = ({ product, handleGetProductDetails }) => {
+ 
   return (
     <div>
       <Card className="w-full max-w-sm mx-auto h-auto bg-[#333333] text-white">
-        <div>
+  
+        <div onClick={() => handleGetProductDetails(product?.id || product?._id)}>
+          
           <div className="relative">
             <img
               src={product?.image}
