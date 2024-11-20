2a646a6b66ae326e102f19413b9be87a03348032
Iyiola afuye <166384569+Iyiola2dev@users.noreply.github.com>
2024-11-20 09:33:54 +0100

    Add product details retrieval: implement getProductDetails function and route

--- server/controllers/shop/products-controller.js
+++ server/controllers/shop/products-controller.js
@@ -60,3 +60,32 @@ export const getFilteredProducts = async (req, res) => {
     });
   }
 };
+
+
+// This is to get product details by id
+export const getProductDetails = async (req, res) => {
+  try {
+    const {id} = req.params;
+    
+
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: product,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+}
