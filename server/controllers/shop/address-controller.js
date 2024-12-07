64de1c6ea699a7f219ae87915cff9c3b9e1196cd
Iyiola afuye <166384569+Iyiola2dev@users.noreply.github.com>
2024-12-07 18:28:20 +0100

    Add address management functionality: implement controller methods, define address model, and set up routes

---
+++ server/controllers/shop/address-controller.js
@@ -0,0 +1,48 @@
+
+
+export const addAddress = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error while adding address",
+      errorMessage: error.message,
+    });
+  }
+};
+
+
+export const fetchAllAddress = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error while adding address",
+      errorMessage: error.message,
+    });
+  }
+};
+
+
+export const editAddress = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error while adding address",
+      errorMessage: error.message,
+    });
+  }
+};
+
+
+export const deleteAddress = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error while adding address",
+      errorMessage: error.message,
+    });
+  }
+};
