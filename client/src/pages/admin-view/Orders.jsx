bbffe2b72ab15c25ac26da57300d7575f2d68738
Iyiola afuye <166384569+Iyiola2dev@users.noreply.github.com>
2024-12-13 03:22:19 +0100

    Replace AdminOrder component with AdminOrders component that integrates AdminOrderView

---
+++ client/src/pages/admin-view/Orders.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import AdminOrderView from "./admin-view2/AdminOrderView";
+
+const AdminOrders = () => {
+  return (
+    <div>
+      <AdminOrderView />
+    </div>
+  );
+};
+
+export default AdminOrders;
