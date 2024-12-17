93a7b44800a9228c9e73eca94ba15a6ae83d1b34
Iyiola afuye <166384569+Iyiola2dev@users.noreply.github.com>
2024-12-17 03:11:46 +0100

    Add loading state to AdminOrderView component

--- client/src/pages/admin-view/admin-view2/AdminOrderView.jsx
+++ client/src/pages/admin-view/admin-view2/AdminOrderView.jsx
@@ -12,9 +12,19 @@ import {
 import { useState } from "react";
 import AdminOrderDetailsView from "./OrderDetails";
 import { Dialog } from "@/components/ui/dialog";
+import { useSelector } from "react-redux";
 
 const AdminOrderView = () => {
   const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
+  const {isLoading}= useSelector((state)=>state.adminProducts)
+  if (isLoading)
+    return (
+      <div className="h-[80vh] flex flex-col items-center justify-center">
+        <p className="text-center font-bold text-3xl">Loading...</p>
+        <div className="mt-4 border-t-4 border-blue-500 border-solid rounded-full w-12 h-12 animate-spin"></div>
+      </div>
+    );
+
   return (
     <Card>
       <CardHeader>
