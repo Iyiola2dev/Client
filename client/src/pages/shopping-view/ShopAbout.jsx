8bf06593995bdd14c44723eb721f6d5c0314eec7
Iyiola afuye <166384569+Iyiola2dev@users.noreply.github.com>
2024-12-17 03:10:12 +0100

    Enhance shopping view layout and add contact information and terms & conditions sections

--- client/src/pages/shopping-view/ShopAbout.jsx
+++ client/src/pages/shopping-view/ShopAbout.jsx
@@ -91,10 +91,87 @@ const ShopAbout = () => {
       </div>
 
       {/* third div */}
-      <div className="flex flex-col-reverse md:flex-row">
-        <span className="flex-1"></span>
-        <span className="flex-1"></span>
+      <div className="flex flex-col-reverse md:flex-row-reverse mt-10 gap-7 items-center xl:gap-[4rem]">
+        <span className="flex-1">
+          <img
+            className=" xl:h-[50rem]"
+            src="https://res.cloudinary.com/dtlejpoxq/image/upload/v1733493467/Mern-Ecommerce/El_vxewzq.png"
+            alt=""
+          />
+        </span>
+        <span className="flex-1 bg-[#D72B7E] text-white p-5 rounded-xl xl:w-[55%] lg:text-xl flex flex-col gap-2">
+          <h2 className="text-center">~ CONS</h2>
+          <ul className="flex flex-col gap-2 md:gap-4 mt-2">
+            <li className="flex gap-2">
+              <p>.</p> <p>Therapists can't prescribe medication.</p>
+            </li>
+            <li className="flex gap-2">
+              <p>.</p> <p>Therapists can't diagnose illnesses.</p>
+            </li>
+            <li className="flex gap-2">
+              <p>.</p>
+              <p>
+                Face-to-face, exchange of contacts or real names by therapist is
+                not allowed 🚫
+              </p>
+            </li>
+            <li className="flex gap-2">
+              <p>.</p> <p>No live chat for assistance on the site</p>
+            </li>
+          </ul>
+        </span>
       </div>
+
+      {/* fourth section */}
+      <section className="mt-10 text-white flex flex-col gap-3">
+        <h3 className="text-2xl font-bold xl:text-3xl">Contact Information</h3>
+        <p className="md:text-lg xl:text-2xl xl:w-[50%]">
+          You may have questions, but we've got you covered. Our team of
+          customer service professionals can communicate with you in English.
+        </p>
+        <p className="text-[#D72B7E] md:text-lg xl:text-xl">
+          E-mail:
+          <a href="mailto:service@Allsextoy.com">service@Allsextoy.com</a>
+        </p>
+      </section>
+
+      {/* FAQ */}
+
+      {/* Terms and conditions */}
+      <section className="mt-10 text-white flex flex-col gap-3">
+        <h3 className="text-xl font-bold xl:text-3xl">Terms and Conditions</h3>
+        <p className="text-lg font-bold md:text-xl xl:text-2xl xl:w-[50%]  mt-3">
+          Introduction
+        </p>
+        <ul>
+          <li className="flex gap-2">
+            <p>-</p>
+            <p className="md:text-lg xl:text-2xl">
+              These terms and conditions outline the rules and regulations for
+              the use of AllSexToys, located at www.allsextoys.online.
+              Intellectual Property Rights
+            </p>
+          </li>
+          <li className="flex gap-2">
+            <p>-</p>
+            <p className="md:text-lg xl:text-2xl">
+              Other than the content you own, under these terms, AllSexToys
+              and/or its licensors own all the intellectual property rights and
+              materials contained in this Website.
+            </p>
+          </li>
+          <p className="text-lg font-bold md:text-xl xl:text-2xl xl:w-[50%]  mt-3">
+            Disclaimer
+          </p>
+          <div className="flex gap-2">
+            <p>-</p>
+            <p className="md:text-lg xl:text-2xl">
+              To the maximum extent permitted by applicable law, we exclude all
+              representations, warranties, and conditions relating to AllSexToys
+            </p>
+          </div>
+        </ul>
+      </section>
     </div>
   );
 };
