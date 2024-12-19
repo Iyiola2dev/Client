b7f4bacb0b5672c515c5b04dc23a430107d44dc1
Iyiola afuye <166384569+Iyiola2dev@users.noreply.github.com>
2024-12-19 19:27:17 +0100

    Add admin order management slice and integrate with Redux store

--- client/src/store/shop/order/index.js
+++ client/src/store/shop/order/index.js
@@ -5,6 +5,8 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 const initialState = {
   orderId: null, // Stores the order details
   paymentUrl: null, // Stores the Paystack payment URL
+  orderList: [],
+  orderDetails: null,
   isLoading: false,
 };
 
@@ -18,24 +20,101 @@ export const createNewOrder = createAsyncThunk(
     return response.data;
   }
 );
+export const capturePayment = createAsyncThunk(
+  "/order/capturePayment",
+  async ({ reference, orderId }) => {
+    const response = await axios.post(
+      "http://localhost:5000/api/shop/order/verify",
+      {
+        reference,
+        orderId,
+      }
+    );
+    return response.data;
+  }
+);
+export const getAllOrdersByUserId = createAsyncThunk(
+  "/order/getAllOrdersByUserId",
+  async (userId) => {
+    const response = await axios.get(
+      `http://localhost:5000/api/shop/order/list/${userId}`
+    );
+    return response.data;
+  }
+);
+export const getOrderDetails = createAsyncThunk(
+  "/order/orderDetails",
+  async (id) => {
+    const response = await axios.get(
+      `http://localhost:5000/api/shop/order/details/${id}`
+    );
+    return response.data;
+  }
+);
 
 const shoppingOrderSlice = createSlice({
   name: "shoppingOrderSlice",
   initialState,
-  reducers: {},
-  extraReducer: (builder) => {
-    builder.addCase(createNewOrder.pending,(state)=>{
-        state.isLoading = true
-    }).addCase(createNewOrder.fulfilled,(state, action)=>{
+  reducers: {
+    resetOrderDetails :(state)=>{
+      state.orderDetails = null
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createNewOrder.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createNewOrder.fulfilled, (state, action) => {
         state.isLoading = false;
         state.orderId = action.payload.orderId;
+        console.log(action.payload.orderId, "action.payload.orderId");
         state.paymentUrl = action.payload.paymentUrl;
-    }).addCase(createNewOrder.rejected,(state)=>{
-        state.isLoading = false
-        state.paymentUrl = null
-        state.orderId = null
-    })
+
+        //This is store the orderId
+        if (action.payload.orderId) {
+          sessionStorage.setItem(
+            "currentOrderId",
+            JSON.stringify(action.payload.orderId)
+          );
+          console.log(
+            "Stored orderId in Session Storage:",
+            sessionStorage.getItem("currentOrderId")
+          ); // Confirm storage
+        } else {
+          console.error("Order ID is undefined or missing from payload");
+        }
+      })
+      .addCase(createNewOrder.rejected, (state) => {
+        state.isLoading = false;
+        state.paymentUrl = null;
+        state.orderId = null;
+      })
+      .addCase(getAllOrdersByUserId.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAllOrdersByUserId.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderList = action.payload.data;
+      })
+      .addCase(getAllOrdersByUserId.rejected, (state) => {
+        state.isLoading = false;
+        state.orderList = [];
+      })
+      .addCase(getOrderDetails.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getOrderDetails.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderDetails = action.payload.data;
+      })
+      .addCase(getOrderDetails.rejected, (state) => {
+        state.isLoading = false;
+        state.orderDetails = null;
+      });
   },
 });
+
+export const {resetOrderDetails} = shoppingOrderSlice.actions;
 
 export default shoppingOrderSlice.reducer;
